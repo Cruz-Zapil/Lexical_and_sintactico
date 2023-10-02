820c3194a0d0e00ea7d573184d2c84abeea21404
Cruz-Zapil <victorzapil202030851@cunoc.edu.gt>
2023-10-02 12:25:08 -0600

    Actuzliando backEnd

--- src/main/java/com/analizador/backEnd/parser/model/funcion/Funcion.java
+++ src/main/java/com/analizador/backEnd/parser/model/funcion/Funcion.java
@@ -1,10 +1,11 @@
 package com.analizador.backEnd.parser.model.funcion;
 
 import com.analizador.backEnd.lexer.Token;
+import com.analizador.backEnd.parser.model.Raiz;
 
 public class Funcion {
 
-    public void scanFuncion(Token lexema, Funcion funcionCalss) {
+    public void scanFuncion(Token lexema, Raiz raiz) {
     }
     
 }
--- src/main/java/com/analizador/backEnd/parser/model/sentencia/Sentencia.java
+++ src/main/java/com/analizador/backEnd/parser/model/sentencia/Sentencia.java
@@ -1,10 +1,11 @@
 package com.analizador.backEnd.parser.model.sentencia;
 
 import com.analizador.backEnd.lexer.Token;
+import com.analizador.backEnd.parser.model.Raiz;
 
 public class Sentencia {
 
-    public void scanSentencia(Token lexema, Sentencia sentenciaClass) {
+    public void scanSentencia(Token lexema, Raiz sentenciaClass) {
 
         
     }
